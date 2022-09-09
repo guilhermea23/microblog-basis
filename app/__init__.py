1000b67dfc4642477ece22e359881393fcc7eac3
guilherme.a.oliveira <garaujodeoliveira1@gmail.com>
2022-09-09 14:14:12 -0300

    🗃️ Pre-configurations to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 Bootstrap(app)
+db = SQLAlchemy(app)
 
-from app import routes
+migrate = Migrate(app,db)
+
+from app import routes, models
