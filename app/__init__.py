bb17f5e656be84ff6df9937b709b6b2d0667a1c9
guilherme.a.oliveira <garaujodeoliveira1@gmail.com>
2022-09-09 12:49:18 -0300

    💄 Add bootstrap integration
    - Use Flask-Bootstrap to style

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+Bootstrap(app)
 
 from app import routes
