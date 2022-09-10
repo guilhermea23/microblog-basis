8804c1f33ce7b285a66c8a14c21b2c8f2e19b6c6
guilhermea23 <garaujodeoliveira1@gmail.com>
2022-09-10 00:26:22 -0300

    :white_check_mark: Finish classes on YouTube
    Nexts steps:
    - Add error flash
    - Add logout button

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 from app.config import Config
 
@@ -11,5 +12,8 @@ Bootstrap(app)
 db = SQLAlchemy(app)
 
 migrate = Migrate(app,db)
+
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,14 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
   user ={'username':'Guilherme'}
   posts = [{
@@ -19,7 +23,22 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 def login():
   form = LoginForm()
-  if form.validate_on_submit():
-    flash(f'Login efetuado com sucesso para {form.username.data}, Remember-me: {form.remember_me.data}')
+  if current_user.is_authenticated:
     return redirect(url_for('index'))
+  if form.validate_on_submit():
+    user = User.query.filter_by(username=form.username.data).first()
+    if user is None or not user.check_password(form.password.data):
+      flash('Login ou senha inválido.')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    next_page = request.args.get('next')
+    if not next_page or url_parse(next_page).netloc == '':
+      next_page = url_for('index')
+    flash(f'Login efetuado com sucesso para {form.username.data}, Remember-me: {form.remember_me.data}')
+    return redirect(next_page)
   return render_template('login.html', form=form, title='Login')
+
+@app.route('/logout')
+def logout():
+  logout_user()
+  return redirect(url_for('index'))
